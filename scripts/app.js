cfdabd8e78d0d0482b1cd21f697030936f8a9438
siva-kumar-python <sivakumar135siva@gmail.com>
2025-06-23 22:16:49 +0530

    add site files

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// Mobile nav toggle
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
